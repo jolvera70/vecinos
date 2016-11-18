95faf00b769c1a630dcea8a7458e28869ae4045d
jolvera <jolvera70@hotmail.com>
2016-11-17 21:34:32 -0600

    #1

---
+++ database/user.sql
@@ -0,0 +1,9 @@
+/*
+-- Query: SELECT * FROM vecinos.users
+LIMIT 0, 1000
+
+-- Date: 2016-11-17 21:32
+*/
+INSERT INTO `users` (`iduser`,`username`,`password`,`name`,`enabled`) VALUES (1,'jolvera','jolvera','jorge',1);
+INSERT INTO `users` (`iduser`,`username`,`password`,`name`,`enabled`) VALUES (2,'bvelasco','bvelasco','Beatriz',1);
+INSERT INTO `users` (`iduser`,`username`,`password`,`name`,`enabled`) VALUES (3,'jorge','jorge','Jorge',1);
