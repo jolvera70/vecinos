c6e4a26c0efe9be661c08443f8fccba4e13caeff
bvelasco77 <beatriz.velasco@eisei.net.mx>
2016-11-20 20:55:25 -0600

    #8

--- database/user_roles.sql
+++
@@ -1,9 +0,0 @@
-/*
--- Query: SELECT * FROM vecinos.user_roles
-LIMIT 0, 1000
-
--- Date: 2016-11-17 21:32
-*/
-INSERT INTO `user_roles` (`user_role_id`,`username`,`rol`) VALUES (1,'jolvera','ROLE_ADMIN');
-INSERT INTO `user_roles` (`user_role_id`,`username`,`rol`) VALUES (2,'bvelasco','ROLE_USER');
-INSERT INTO `user_roles` (`user_role_id`,`username`,`rol`) VALUES (3,'jorge','ROLE_ROOT');
